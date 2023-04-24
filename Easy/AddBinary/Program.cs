3c12a05611e231cd4f2aed548d10a6d789b31614
Nambukarthy Raveendran <nambukarthy.raveendran@in.abb.com>
2023-04-24 19:52:54 +0530

    [Easy] Add Binary, [Medium] LongestPalindromicSubstring (Incomplete).

---
+++ Easy/AddBinary/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* ----- Description ----- */
+/*
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+Constraints:
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+*/
+
+namespace AddBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(addBinary("1", "111"));
+            Console.ReadKey();
+        }
+
+
+        //Worst Possible Solution.
+        /// <summary>
+        /// Solution One
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        /*
+        static string addBinary(string a, string b)
+        {
+           var aArray =  a.ToCharArray().Reverse().ToList();
+           var bArray = b.ToCharArray().Reverse().ToList();
+
+           int count = 0;
+        
+           string result = string.Empty;
+           bool isCarryForward = false;
+
+           while (count < aArray.Count || count < bArray.Count)
+           {
+               if (count < aArray.Count && count < bArray.Count)
+               {
+                   if (aArray[count] == '0' && bArray[count] == '0')
+                   {
+                       if (isCarryForward)
+                       {
+                           result = "1" + result;
+                           isCarryForward = false;
+                       }
+                       else
+                           result = "0" + result;
+                   }
+                   else if (aArray[count] == '1' && bArray[count] == '0')
+                   {
+                       if (isCarryForward)
+                       {
+                           result = "0" + result;
+                           isCarryForward = true;
+                       }
+                       else
+                         result = "1" + result;
+                   }
+                   else if (aArray[count] == '0' && bArray[count] == '1')
+                   {
+                       if (isCarryForward)
+                       {
+                           result = "0" + result;
+                           isCarryForward = true;
+                       }
+                       else
+                           result = "1" + result;
+                   }
+                   else if (aArray[count] == '1' && bArray[count] == '1')
+                   {
+                       if (isCarryForward)
+                       {
+                           result = "1" + result;
+                       }
+                       else
+                       {
+                           result = "0" + result;
+                       }
+                       
+                       isCarryForward = true;
+                   }
+               }
+
+               if (count < aArray.Count && count >= bArray.Count)
+               {
+                   if (aArray[count] == '1' && isCarryForward)
+                   {
+                       result = "0" + result;
+                   }
+                   else if (aArray[count] == '0' && isCarryForward)
+                   {
+                       result = "1" + result;
+                       isCarryForward = false;
+                   }
+                   else
+                   {
+                       result = aArray[count] + result;
+                   }
+               }
+
+               if (count >= aArray.Count && count < bArray.Count)
+               {
+                   if (bArray[count] == '1' && isCarryForward)
+                   {
+                       result = "0" + result;
+                   }
+                   else if (bArray[count] == '0' && isCarryForward)
+                   {
+                       result = "1" + result;
+                       isCarryForward = false;
+                   }
+                   else
+                   {
+                       result = bArray[count] + result;
+                   }
+               }
+
+               count++;
+           }
+
+           if (isCarryForward)
+           {
+               result = "1" + result;
+           }
+
+           return result;
+        }
+        */
+
+
+        //Little better but still not good solution, have plenty of room to optimize. 
+        //TODO: Need to come up with better logic. 
+        /// <summary>
+        /// Solution Two
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static string addBinary(string a, string b)
+        {
+            string result = string.Empty;
+            bool isCarryForward = false;
+
+            int count = (a.Length >= b.Length ? a.Length : b.Length) - 1;
+            int aCount = a.Length - 1;
+            int bCount = b.Length - 1;
+
+            for (int i = count; i >= 0; i--)
+            {
+                if (aCount >= 0 && bCount >= 0)
+                {
+                    if (a[aCount] == b[bCount])
+                    {
+                        if (isCarryForward)
+                        {
+                            result = "1" + result;
+                            isCarryForward = false;
+                        }
+                        else
+                        {
+                            result = "0" + result;
+                        }
+
+                        isCarryForward = a[aCount] == '1';
+                    }
+
+                    else if (a[aCount] != b[bCount])
+                    {
+                        if (isCarryForward)
+                        {
+                            result = "0" + result;
+                            isCarryForward = true;
+                        }
+                        else
+                            result = "1" + result;
+                    }
+                }
+
+                if (aCount >= 0 && bCount < 0)
+                {
+                    if (a[aCount] == '1' && isCarryForward)
+                    {
+                        result = "0" + result;
+                    }
+                    else if (a[aCount] == '0' && isCarryForward)
+                    {
+                        result = "1" + result;
+                        isCarryForward = false;
+                    }
+                    else
+                    {
+                        result = a[aCount] + result;
+                    }
+                }
+
+                else if (bCount >= 0 && aCount < 0)
+                {
+                    if (b[bCount] == '1' && isCarryForward)
+                    {
+                        result = "0" + result;
+                    }
+                    else if (b[bCount] == '0' && isCarryForward)
+                    {
+                        result = "1" + result;
+                        isCarryForward = false;
+                    }
+                    else
+                    {
+                        result = b[bCount] + result;
+                    }
+                }
+
+                count--;
+                aCount--;
+                bCount--;
+            }
+
+            if (isCarryForward)
+            {
+                result = "1" + result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Medium/LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* ----- Description ----- */
+/*
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+ 
+Constraints:
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+*/
+
+namespace LongestPalindromicSubstring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(longestPalindrome("bacabab"));
+            Console.ReadKey();
+        }
+
+        static string longestPalindrome(string input)
+        {
+            if (IsPalindrome(input)) return input;
+
+            //List<Palindrome> pal = new List<Palindrome>();
+           
+            int maxPalLength = 0;
+            string pal = string.Empty;
+            int inputLength = input.Length;
+           
+            for (int i = 1; i < inputLength; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    var str = input.Substring(j, (i-j)+1);
+
+                    if (IsPalindrome(str))
+                    {
+                        if (str.Length > maxPalLength)
+                        {
+                            maxPalLength = str.Length;
+                            pal = str;
+                        }
+                    }
+                    //pal.Add(new Palindrome() { PalindromeLength = str.Length, PalindromeString = str });
+
+                    if ((inputLength / 2) <= maxPalLength)
+                        goto LoopEnd;
+                }
+            }
+
+            LoopEnd:
+
+            //var result = pal.OrderByDescending(x => x.PalindromeLength).FirstOrDefault();
+            //if (result == null) return input[0].ToString();
+            //return result.PalindromeString;
+
+            if (string.IsNullOrEmpty(pal)) return input.Substring(0, 1);
+            return pal;
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            input = input.ToLower();
+            string reversedInput = string.Empty;
+            for (int i = input.Length; i > 0; i--)
+            {
+                reversedInput += input[i-1];
+            }
+            return reversedInput == input;
+        }
+    }
+
+    public class Palindrome
+    {
+        public int PalindromeLength  { get; set; }
+        public string PalindromeString { get; set; }
+    }
+}
