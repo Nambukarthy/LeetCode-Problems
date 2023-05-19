ebde1e58182167257aef347c47bf8fb78a83465f
Nambukarthy Raveendran <nambukarthy.raveendran@in.abb.com>
2023-05-19 16:58:25 +0530

    [Medium] LongestPalindromicSubstring.

--- Medium/LongestPalindromicSubstring/Program.cs
+++ Medium/LongestPalindromicSubstring/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /* ----- Description ----- */
 /*
@@ -28,25 +24,69 @@ namespace LongestPalindromicSubstring
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(longestPalindrome("bacabab"));
+            //Console.WriteLine(AddEvenApproach("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"));
+            Console.WriteLine(AddEvenApproach("nythiknambukarthyraveendran"));
             Console.ReadKey();
         }
 
+        //Add Even approach. 
+        static string AddEvenApproach(string s)
+        {
+            int length = s.Length;
+            int longest = 0;
+            string result = string.Empty;
+
+            for (int i = 0; i < length; i++)
+            {
+                int oddGap = 1;
+                int evenGap = 1;
+
+                while ((i + oddGap) < length && (i - oddGap) >= 0 && (s[i + oddGap] == s[i - oddGap]))
+                {
+                    oddGap++;
+                }
+                oddGap--;
+
+                while ((i + evenGap) < length && (i - (evenGap - 1)) >= 0 && (s[i + evenGap] == s[i - (evenGap - 1)]))
+                {
+                    evenGap++;
+                }
+                evenGap--;
+
+                if (longest < ((oddGap * 2) + 1) || longest < (evenGap * 2))
+                {
+                    if (oddGap >= evenGap)
+                    {
+                        longest = (oddGap * 2) + 1;
+                        result = s.Substring(i - oddGap, longest);
+                    }
+                    else
+                    {
+                        longest = evenGap * 2;
+                        result = s.Substring((i + 1) - evenGap, longest);
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(result)) result = s[0].ToString();
+            return result;
+        }
+
+        //Nestedloop - Solutions. 
         static string longestPalindrome(string input)
         {
             if (IsPalindrome(input)) return input;
 
-            //List<Palindrome> pal = new List<Palindrome>();
-           
             int maxPalLength = 0;
             string pal = string.Empty;
             int inputLength = input.Length;
-           
-            for (int i = 1; i < inputLength; i++)
+
+            //Nested loop solution - 1 
+            /*for (int i = 0; i < inputLength; i++)
             {
-                for (int j = 0; j < i; j++)
+                for (int j = i + 1; j <= inputLength; j++)
                 {
-                    var str = input.Substring(j, (i-j)+1);
+                    var str = input.Substring(i, j - i);
 
                     if (IsPalindrome(str))
                     {
@@ -56,38 +96,39 @@ namespace LongestPalindromicSubstring
                             pal = str;
                         }
                     }
-                    //pal.Add(new Palindrome() { PalindromeLength = str.Length, PalindromeString = str });
+                }
+            }*/
 
-                    if ((inputLength / 2) <= maxPalLength)
-                        goto LoopEnd;
+            // Nested loop solution  2 
+            for (int i = 1; i < inputLength; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    var str = input.Substring(j, (i - j) + 1);
+
+                    if (IsPalindrome(str))
+                    {
+                        if (str.Length > maxPalLength)
+                        {
+                            maxPalLength = str.Length;
+                            pal = str;
+                        }
+                    }
                 }
             }
 
-            LoopEnd:
-
-            //var result = pal.OrderByDescending(x => x.PalindromeLength).FirstOrDefault();
-            //if (result == null) return input[0].ToString();
-            //return result.PalindromeString;
-
             if (string.IsNullOrEmpty(pal)) return input.Substring(0, 1);
             return pal;
-        }
-
+        }       
         static bool IsPalindrome(string input)
         {
             input = input.ToLower();
             string reversedInput = string.Empty;
             for (int i = input.Length; i > 0; i--)
             {
-                reversedInput += input[i-1];
+                reversedInput += input[i - 1];
             }
             return reversedInput == input;
         }
-    }
-
-    public class Palindrome
-    {
-        public int PalindromeLength  { get; set; }
-        public string PalindromeString { get; set; }
     }
 }
